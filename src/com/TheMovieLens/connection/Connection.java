7d1fc5dc79a1a39795bf35b12646c28d51fa1006
degrandelucas <lucasdegrande15@gmail.com>
2024-12-21 00:11:35 -0300

    corrigindo problemas na classe Connection

--- src/com/TheMovieLens/connection/Connection.java
+++ src/com/TheMovieLens/connection/Connection.java
@@ -9,22 +9,26 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class Connection {
-    String url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+    static String urlAPI = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
 
-    HttpClient client = HttpClient.newBuilder().build();
+    public static String connection() {
 
-    try{
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                        .header("accept", "application/json")
-                        .header("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjBlNGI4ZmU3YjY1OGE1ZWEzZTA1MjNiOTkyM2NhOSIsIm5iZiI6MTczNDcxOTgzOS4wMTEwMDAyLCJzdWIiOiI2NzY1Yjk1ZjkxOTI4N2VmNTM5MGRiZWMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.KgZKTDzPzzJ9dvT2Np0vsqhI8hIVKxf9OyXE0mUY48s")
-                        .GET()
-                        .build();
+        HttpClient client = HttpClient.newBuilder().build();
 
-        HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString()).execute();
-    } catch(IOException | InterruptedException e) {
-        throw new RuntimeException(e);
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(urlAPI))
+                    .header("accept", "application/json")
+                    .header("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjBlNGI4ZmU3YjY1OGE1ZWEzZTA1MjNiOTkyM2NhOSIsIm5iZiI6MTczNDcxOTgzOS4wMTEwMDAyLCJzdWIiOiI2NzY1Yjk1ZjkxOTI4N2VmNTM5MGRiZWMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.KgZKTDzPzzJ9dvT2Np0vsqhI8hIVKxf9OyXE0mUY48s")
+                    .GET()
+                    .build();
+
+            HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return String.valueOf(response.body());
+        } catch (IOException |
+                 InterruptedException e) {
+            throw new RuntimeException("Erro na conexao" + e);
+        }
     }
-    
 
 }
