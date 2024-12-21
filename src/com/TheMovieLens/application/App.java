b26320e2aa8b49b7a17d5193aaca61095a954cec
degrandelucas <lucasdegrande15@gmail.com>
2024-12-21 00:17:03 -0300

    add Class Movie RecordDto to standardize

--- src/com/TheMovieLens/application/App.java
+++ src/com/TheMovieLens/application/App.java
@@ -1,8 +1,11 @@
 package com.TheMovieLens.application;
 
+import com.TheMovieLens.connection.Connection;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Segue lista de Filmes mais Populares do momento");
-
+        String teste = Connection.connection();
+        System.out.println(teste);
     }
 }
---
+++ src/com/TheMovieLens/dto/MoviesDto.java
@@ -0,0 +1,4 @@
+package com.TheMovieLens.dto;
+
+public record MoviesDto(String original_title, String title, String release_date, Double vote_average) {
+}
