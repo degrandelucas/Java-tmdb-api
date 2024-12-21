44f07e85edfbe78c67c946dc3cceea12346b4411
degrandelucas <lucasdegrande15@gmail.com>
2024-12-21 00:19:42 -0300

    add comment to treat dates later

--- src/com/TheMovieLens/application/App.java
+++ src/com/TheMovieLens/application/App.java
@@ -5,7 +5,7 @@ import com.TheMovieLens.connection.Connection;
 public class App {
     public static void main(String[] args) {
         System.out.println("Segue lista de Filmes mais Populares do momento");
-        String teste = Connection.connection();
+        String teste = Connection.connection(); //teste, realizar o tratamento posteriormente
         System.out.println(teste);
     }
 }
