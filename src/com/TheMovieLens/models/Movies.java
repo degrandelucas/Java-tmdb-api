9620750b799d16241345c3de71537985168f2465
degrandelucas <lucasdegrande15@gmail.com>
2024-12-23 22:44:19 -0300

    Exclusao class Movie nao utilizada no momento

--- src/com/TheMovieLens/models/Movies.java
+++
@@ -1,8 +0,0 @@
-package com.TheMovieLens.models;
-
-public class Movies {
-    private String movieTitle;
-    private String originalTitle;
-    private double movieDate;
-    private double mediaAvarageMovie;
-}
