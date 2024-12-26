4909924c48f62ac2723b5cf50b077a3831ada356
degrandelucas <lucasdegrande15@gmail.com>
2024-12-26 15:07:47 -0300

    change var jsonMoviesDto name in the place teste

--- src/com/TheMovieLens/application/App.java
+++ src/com/TheMovieLens/application/App.java
@@ -8,17 +8,16 @@ import com.TheMovieLens.models.APITreatment;
 public class App {
     public static void main(String[] args) {
         System.out.println("Segue lista de Filmes mais Populares do momento");
-        String PopularMoviesFullAPI = Connection.connection(); //teste, realizar o tratamento posteriormente
+        String PopularMoviesFullAPI = Connection.connection(); //jsonMoviesDto, realizar o tratamento posteriormente
         System.out.println(PopularMoviesFullAPI);
 
         //Campos que queremos manter no resultado
-        APITreatment chooseFieldsMovies = Conversion.conversionFromJson(PopularMoviesFullAPI);
-        System.out.println(chooseFieldsMovies);
+        APITreatment TreatmentFieldsMovies = Conversion.conversionFromJson(PopularMoviesFullAPI);
+        System.out.println(TreatmentFieldsMovies);
 
-        String teste = Conversion.conversionToJson(chooseFieldsMovies);
-        System.out.println(teste);
-
-
+        String jsonMoviesDto = Conversion.conversionToJson(TreatmentFieldsMovies);
+        System.out.println(jsonMoviesDto);
+        
         //Converter a string obtida da API JSON para um JsonArray
         //JsonArray jsonArray = JsonParser.parseString(json).getAsJsonArray();
 
