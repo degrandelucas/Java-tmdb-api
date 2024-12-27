2c1fd023451bbf4daadbd6da4747d8b178592abf
degrandelucas <lucasdegrande15@gmail.com>
2024-12-27 13:33:52 -0300

    Metodos para obter os titulos e os caminhos dos posteres, substituindo os metodos criados com forEach

---
+++ src/com/TheMovieLens/dto/Movie.java
@@ -0,0 +1,21 @@
+package com.TheMovieLens.dto;
+
+import com.google.gson.annotations.SerializedName;
+
+public record Movie(
+        String title,
+        @SerializedName("original_title") String originalTitle,
+        @SerializedName("poster_path") String posterPath,
+        @SerializedName("vote_average") double voteAverage,
+        int releaseYear // Este é calculado, entao nao precisa do mapeamento
+) {
+    public Movie(String title, String originalTitle, String posterPath, double voteAverage, String releaseDate) {
+        this(
+                title,
+                originalTitle,
+                posterPath,
+                voteAverage,
+                Integer.parseInt(releaseDate.substring(0, 4))
+        );
+    }
+}
--- src/com/TheMovieLens/models/APITreatment.java
+++ src/com/TheMovieLens/models/APITreatment.java
@@ -1,54 +1,27 @@
 package com.TheMovieLens.models;
 
-import com.TheMovieLens.dto.MoviesDto;
+import com.TheMovieLens.dto.Movie;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class APITreatment {
     private int page;
-    private List<MoviesDto> results; //results e o nome do array de resultados dos filmes na API
-    private List<String> titleListMovies = new ArrayList<>();;
-    private List<String> posterListURL = new ArrayList<>();;
+    private List<Movie> results; //results e o nome do array de resultados dos filmes na API
+    private List<String> titleListMovies = new ArrayList<>();
+    private List<String> posterListURL = new ArrayList<>();
 
-    public void setResults(List<MoviesDto> results) {
+    public void setResults(List<Movie> results) {
         this.results = results;
     }
 
-    public void titleList() {
-        System.out.println("Lista de títulos:");
-        for (MoviesDto movie : results) {
-            System.out.println(movie.title());
-            titleListMovies.add(movie.title());
-        }
+    // Metodos para obter os titulos e os caminhos dos posteres, substituindo os metodos criados com forEach
+    public List<String> getTitles() {
+        return results.stream().map(Movie::title).toList();  // Extrai os títulos dos filmes
     }
 
-    public void posterUrl() {
-        System.out.println("Lista dos posters:");
-        for (MoviesDto posters : results) {
-            System.out.println(posters.poster_path());
-            posterListURL.add(posters.poster_path());
-        }
-    }
-
-    public List<String> getTitleListMovies() {
-        return titleListMovies;
-    }
-
-    public List<String> getPosterListURL() {
-        return posterListURL;
-    }
-
-    public void setTitleListMovies() {
-        for (MoviesDto movie : results) {
-            titleListMovies.add(movie.title());
-        }
-    }
-
-    public void setPosterListURL() {
-        for (MoviesDto posters : results) {
-            posterListURL.add(posters.poster_path());
-        }
+    public List<String> getPosterPaths() {
+        return results.stream().map(Movie::posterPath).toList();  // Extrai os caminhos dos pôsteres
     }
 
     @Override
