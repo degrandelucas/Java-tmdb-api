bfb8c48e5c5cbc057e8ccbd0ff2d96ed620933ae
degrandelucas <lucasdegrande15@gmail.com>
2025-01-02 19:02:17 -0300

    bug: string format to correct

--- src/com/TheMovieLens/web/HTMLGenerator.java
+++ src/com/TheMovieLens/web/HTMLGenerator.java
@@ -5,6 +5,8 @@ import com.TheMovieLens.models.APITreatment;
 import java.io.PrintWriter;
 import java.util.List;
 
+import static java.lang.String.format;
+
 public class HTMLGenerator {
     private PrintWriter writer;
 
@@ -30,16 +32,31 @@ public class HTMLGenerator {
 
         // Adiciona os filmes ao HTML
         for (int i = 0; i < titles.size(); i++) {
+
+            writer.println(format("""
+                    <div style='margin-bottom: 20px;'>
+                    <h2> %s </h2>
+                    <img src='https://image.tmdb.org/t/p/w200+ %s alt='Poster' style='width:200px;'>
+                    <p><strong>Nota:</strong>  %d </p>
+                    <p><strong>Ano:</strong> %s </p>
+                    """,
+                    titles.get(i),
+                    posterPaths.get(i),
+                    votes.get(i),
+                    year.get(i)));
+
             writer.println("<div style='margin-bottom: 20px;'>");
             writer.println("<h2>" + titles.get(i) + "</h2>");
             writer.println("<img src='https://image.tmdb.org/t/p/w200" + posterPaths.get(i) + "' alt='Poster' style='width:200px;'>");
             writer.println("<p><strong>Nota:</strong> " + votes.get(i) + "</p>");
             writer.println("<p><strong>Ano:</strong> " + year.get(i) + "</p>");
             writer.println("</div>");
+
         }
 
         // Fechamento do HTML
         writer.println("</body>");
         writer.println("</html>");
+
     }
 }
